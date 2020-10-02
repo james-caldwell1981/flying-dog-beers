078fb0aba9a284a62b37d5b89a1aac2840d31176
james-caldwell1981 <66021725+james-caldwell1981@users.noreply.github.com>
2020-10-01 22:54:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
+beers=['Beer!', 'Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[99, 13, 60, 85, 75]
+abv_values=[65, 5.0, 5.1, 9.2, 4.3]
 color1='red'
 color2='yellow'
 mytitle='Beer Comparison'
